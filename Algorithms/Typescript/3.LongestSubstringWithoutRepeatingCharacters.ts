e20da0c13bcfe85bc36f636464b1326eab2695e8
Hadid Ramadhan <137972935+InitialH14@users.noreply.github.com>
2025-09-16 22:58:36 +0700

    Update 3.LongestSubstringWithoutRepeatingCharacters.ts

--- Algorithms/Typescript/3.LongestSubstringWithoutRepeatingCharacters.ts
+++ Algorithms/Typescript/3.LongestSubstringWithoutRepeatingCharacters.ts
@@ -1,3 +1,28 @@
+// My Submission : https://leetcode.com/problems/longest-substring-without-repeating-characters/submissions/1772916803
+// Author : InitialH14 a.k.a Hadid
+// Date   : 2025-09-16
+
+/**********************************************************************
+Given a string s, find the length of the longest substring without duplicate characters.
+
+Example 1:
+
+Input: s = "abcabcbb"
+Output: 3
+Explanation: The answer is "abc", with the length of 3.
+
+Example 2:
+
+Input: s = "bbbbb"
+Output: 1
+Explanation: The answer is "b", with the length of 1.
+
+Input: s = "pwwkew"
+Output: 3
+Explanation: The answer is "wke", with the length of 3.
+Notice that the answer must be a substring, "pwke" is a subsequence and not a substring.
+**********************************************************************/
+
 function lengthOfLongestSubstring(s: string): number {
     let set = new Set<string>();
     let left = 0;
